ee30bef034a39c4d0e14b95a66b20825bedd552d
Bailly Romain <rbailly17000@gmail.com>
2019-07-23 19:59:00 +0200

    Fix FMT in social module

--- social/twitter_test.go
+++ social/twitter_test.go
@@ -16,16 +16,16 @@ type assert struct {
 
 func TestValidate(T *testing.T) {
 	assertions := []assert{
-		assert{arg: "", expected: fmt.Errorf(" is too short")},
-		assert{arg: "this-string-has-too-much-runes", expected: fmt.Errorf("this-string-has-too-much-runes is too long")},
-		assert{arg: "TWITTER", expected: fmt.Errorf("TWITTER contains illegal pattern")},
-		assert{arg: "Twitter", expected: fmt.Errorf("Twitter contains illegal pattern")},
-		assert{arg: "twitter", expected: fmt.Errorf("twitter contains illegal pattern")},
-		assert{arg: "twittEr", expected: fmt.Errorf("twittEr contains illegal pattern")},
-		assert{arg: "TWITTTT", expected: nil},
-		assert{arg: "This_is_Valid", expected: nil},
-		assert{arg: "-illegal-rune", expected: fmt.Errorf("-illegal-rune contains illegal characters")},
-		assert{arg: "000__000", expected: nil},
+		{arg: "", expected: fmt.Errorf(" is too short")},
+		{arg: "this-string-has-too-much-runes", expected: fmt.Errorf("this-string-has-too-much-runes is too long")},
+		{arg: "TWITTER", expected: fmt.Errorf("TWITTER contains illegal pattern")},
+		{arg: "Twitter", expected: fmt.Errorf("Twitter contains illegal pattern")},
+		{arg: "twitter", expected: fmt.Errorf("twitter contains illegal pattern")},
+		{arg: "twittEr", expected: fmt.Errorf("twittEr contains illegal pattern")},
+		{arg: "TWITTTT", expected: nil},
+		{arg: "This_is_Valid", expected: nil},
+		{arg: "-illegal-rune", expected: fmt.Errorf("-illegal-rune contains illegal characters")},
+		{arg: "000__000", expected: nil},
 	}
 
 	for _, assertion := range assertions {
