a6b6a49df9b693ddd09debbb6c55bf97e071a01a
Bailly Romain <rbailly17000@gmail.com>
2019-07-25 07:33:56 +0200

    Implement Github.IsAvailable

--- github/available.go
+++ github/available.go
@@ -1,6 +1,30 @@
 package github
 
-// IsAvailable check if the given name is available in Twitter
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	curl "github.com/barolab/candidate/url"
+)
+
+// IsAvailable check if the given name is available in Github
 func (g *Github) IsAvailable(name string) (bool, error) {
-	return false, nil
+	u, err := curl.WithoutQuery(fmt.Sprintf("https://github.com/%s", url.QueryEscape(name)))
+	if err != nil {
+		return false, err
+	}
+
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return false, fmt.Errorf("Failed to build HTTP request for URL %s, got error: %s", u, err)
+	}
+
+	res, err := g.client.Fetch(req)
+	if err != nil {
+		return false, fmt.Errorf("Failed to contact %s at %s with error %s", g, u, err)
+	}
+
+	defer res.Body.Close()
+	return res.StatusCode == http.StatusNotFound, nil
 }
